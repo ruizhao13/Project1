39bb12344f609b75c3bcc7117501414e9133fd73
ruizhao <zr1998@mail.ustc.edu.cn>
2018-03-25 21:03:00 +0800

    判断文件是否读完改善

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -82,91 +82,7 @@ void EnterMap(string last_word, string current_word)
 	}
 }
 
-//void NumOfCharsLinesInFile(string FileLocation)
-//{//读入文件，统计字符数、行数、单词数，并加入到全局变量中。并对单词进行处理，加入map字典中。
-//	int NumberChars = 0;
-//	int NumberLines = 0;
-//	int NumberWords = 0;
-//	char last_char = ' ';
-//	char current_char;
-//	bool wordbegin = false;
-//	string current_word;
-//	string last_word;
-//	ifstream infile;
-//	infile.open(FileLocation, ifstream::in);
-//	while (infile.good())
-//	{
-//		current_char = infile.get();
-//		if (current_char == -1) {
-//			break;
-//		}
-//		if (current_char < -1 || current_char>255)
-//		{
-//			current_char = ' ';
-//		}
-//		//判断是否为单词
-//		if ((!isalpha(last_char)) && (!isdigit(last_char)) && (isalpha(current_char)))
-//		{
-//			wordbegin = true;
-//			current_word = current_char;
-//		}
-//		else if (wordbegin)
-//		{
-//			if ((isalpha(current_char)) || (isdigit(current_char)))
-//			{
-//				current_word = current_word + current_char;
-//			}
-//			else
-//			{
-//				wordbegin = false;
-//				//判断现在的current_word是否满足word的要求：前四个字符都是字母
-//				if (isalpha(current_word[0]))
-//				{
-//					if (isalpha(current_word[1]))
-//					{
-//						if (isalpha(current_word[2]))
-//						{
-//							if (isalpha(current_word[3]))
-//							{
-//								//说明current_word满足要求
-//								//cout << current_word << endl;
-//								NumberWords++;
-//								EnterMap(last_word, current_word);
-//								last_word = current_word;
-//								current_word.clear();
-//							}
-//						}
-//					}
-//				}
-//
-//				//如果满足word要求，则将NumberWords++，并处理该word，并last_word=current_word
-//
-//
-//				//将current_word清空
-//			}
-//		}
-//
-//
-//
-//
-//		//判断是否为单词结束
-//		NumberChars++;
-//		if (current_char == '\n') {
-//			NumberLines++;
-//		}
-//		last_char = current_char;
-//	}
-//	infile.close();
-//	if (NumberChars != 0) {
-//		NumberLines++;
-//	}
-//	TotalNum_chars += NumberChars;
-//	TotalNum_lines += NumberLines;
-//	TotalNum_words += NumberWords;
-//	//
-//}
 
-//============================================================
 void NumOfCharsLinesInFile(string FileLocation)
 {//读入文件，统计字符数、行数、单词数，并加入到全局变量中。并对单词进行处理，加入map字典中。
 	int NumberChars = 0;
@@ -178,7 +94,7 @@ void NumOfCharsLinesInFile(string FileLocation)
 	string current_word;
 	string last_word;
 	freopen(FileLocation.c_str(), "r", stdin);
-	while ((current_char=getchar())!=EOF)
+	while (~(current_char=getchar()))
 	{
 		
 		if (current_char == -1) {
