62e5acda535c71d18d6d85bdf1af5019bcd153a5
ruizhao <zr1998@mail.ustc.edu.cn>
2018-03-25 20:58:50 +0800

    读文件方式改变

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -6,6 +6,7 @@
 #include<map>
 #include <algorithm>
 #include <unordered_map>
+#include <time.h>
 using namespace std;
 long long TotalNum_chars = 0;
 long long TotalNum_lines = 0;
@@ -81,6 +82,91 @@ void EnterMap(string last_word, string current_word)
 	}
 }
 
+//void NumOfCharsLinesInFile(string FileLocation)
+//{//读入文件，统计字符数、行数、单词数，并加入到全局变量中。并对单词进行处理，加入map字典中。
+//	int NumberChars = 0;
+//	int NumberLines = 0;
+//	int NumberWords = 0;
+//	char last_char = ' ';
+//	char current_char;
+//	bool wordbegin = false;
+//	string current_word;
+//	string last_word;
+//	ifstream infile;
+//	infile.open(FileLocation, ifstream::in);
+//	while (infile.good())
+//	{
+//		current_char = infile.get();
+//		if (current_char == -1) {
+//			break;
+//		}
+//		if (current_char < -1 || current_char>255)
+//		{
+//			current_char = ' ';
+//		}
+//		//判断是否为单词
+//		if ((!isalpha(last_char)) && (!isdigit(last_char)) && (isalpha(current_char)))
+//		{
+//			wordbegin = true;
+//			current_word = current_char;
+//		}
+//		else if (wordbegin)
+//		{
+//			if ((isalpha(current_char)) || (isdigit(current_char)))
+//			{
+//				current_word = current_word + current_char;
+//			}
+//			else
+//			{
+//				wordbegin = false;
+//				//判断现在的current_word是否满足word的要求：前四个字符都是字母
+//				if (isalpha(current_word[0]))
+//				{
+//					if (isalpha(current_word[1]))
+//					{
+//						if (isalpha(current_word[2]))
+//						{
+//							if (isalpha(current_word[3]))
+//							{
+//								//说明current_word满足要求
+//								//cout << current_word << endl;
+//								NumberWords++;
+//								EnterMap(last_word, current_word);
+//								last_word = current_word;
+//								current_word.clear();
+//							}
+//						}
+//					}
+//				}
+//
+//				//如果满足word要求，则将NumberWords++，并处理该word，并last_word=current_word
+//
+//
+//				//将current_word清空
+//			}
+//		}
+//
+//
+//
+//
+//		//判断是否为单词结束
+//		NumberChars++;
+//		if (current_char == '\n') {
+//			NumberLines++;
+//		}
+//		last_char = current_char;
+//	}
+//	infile.close();
+//	if (NumberChars != 0) {
+//		NumberLines++;
+//	}
+//	TotalNum_chars += NumberChars;
+//	TotalNum_lines += NumberLines;
+//	TotalNum_words += NumberWords;
+//	//
+//}
+
+//============================================================
 void NumOfCharsLinesInFile(string FileLocation)
 {//读入文件，统计字符数、行数、单词数，并加入到全局变量中。并对单词进行处理，加入map字典中。
 	int NumberChars = 0;
@@ -91,11 +177,10 @@ void NumOfCharsLinesInFile(string FileLocation)
 	bool wordbegin = false;
 	string current_word;
 	string last_word;
-	ifstream infile;
-	infile.open(FileLocation, ifstream::in);
-	while (infile.good())
+	freopen(FileLocation.c_str(), "r", stdin);
+	while ((current_char=getchar())!=EOF)
 	{
-		current_char = infile.get();
+		
 		if (current_char == -1) {
 			break;
 		}
@@ -155,7 +240,7 @@ void NumOfCharsLinesInFile(string FileLocation)
 		}
 		last_char = current_char;
 	}
-	infile.close();
+	
 	if (NumberChars != 0) {
 		NumberLines++;
 	}
@@ -164,7 +249,6 @@ void NumOfCharsLinesInFile(string FileLocation)
 	TotalNum_words += NumberWords;
 	//
 }
-
 
 //深度优先递归遍历当前目录下文件夹和文件及子文件夹和文件  
 void DfsFolder(string path, int layer)
@@ -256,6 +340,7 @@ void Getten_phrase()
 
 int main(int argc, char *argv[])
 {
+	clock_t tStart = clock();
 	//递归遍历文件夹  
 	DfsFolder("D:/newsample", 0);
 	//递归遍历文件夹结束
@@ -276,5 +361,6 @@ int main(int argc, char *argv[])
 		cout << ten_phrase[i].sort_phrase << "  " << ten_phrase[i].appear_count << endl;
 
 	}
+	printf("Time taken: %.2fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
 	return 0;
 }
