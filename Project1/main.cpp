1b4137d7f0a9d43dc76a19c1212e0cb8624ace9e
ruizhao <zr1998@mail.ustc.edu.cn>
2018-03-26 23:20:57 +0800

    if嵌套优化

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -170,23 +170,15 @@ void NumOfCharsLinesInFile(string FileLocation)
 			{
 				wordbegin = false;
 				//判断现在的current_word是否满足word的要求：前四个字符都是字母
-				if (isalpha(current_word[0]))
+				if (isalpha(current_word[0])&& isalpha(current_word[1]) && isalpha(current_word[2]) && isalpha(current_word[3]))
 				{
-					if (isalpha(current_word[1]))
-					{
-						if (isalpha(current_word[2]))
-						{
-							if (isalpha(current_word[3]))
-							{
-								//说明current_word满足要求
-								//cout << current_word << endl;
-								NumberWords++;
-								EnterMap(last_word, current_word);
-								last_word = current_word;
-								current_word.clear();
-							}
-						}
-					}
+					
+					//说明current_word满足要求
+					NumberWords++;
+					EnterMap(last_word, current_word);
+					last_word = current_word;
+					current_word.clear();
+					
 				}
 
 				//如果满足word要求，则将NumberWords++，并处理该word，并last_word=current_word
